7359e38e3bac499326fd1fab4926d5aa835585f7
steven <steven.spicher@gmail.com>
2024-09-19 09:27:37 -0600

    Add new assets and update UI components
    Added a new SVG logo and images to the assets folder. Enhanced the App, Calendar, MapPage, and Rental components with new functionality, styles, and navigation logic. Included configuration and dependencies for a new hosting setup using Vite.

--- smith_apts/src/Components/Calendar.tsx
+++ smith_apts/src/Components/Calendar.tsx
@@ -1,41 +1,41 @@
 import {Button, Grid, Stack} from "@mui/material";
-import {DateRangePicker} from "react-date-range";
+// import {DateRangePicker} from "react-date-range";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import {useState} from "react";
+// import {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {addDays, isSameDay} from "date-fns";
+// import {addDays, isSameDay} from "date-fns";
 
 const Calendar = () => {
     const navigate = useNavigate();
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(addDays(new Date(), 7));
-    const selectionRange : any = {
-        startDate: startDate,
-        endDate: endDate,
-        key: "selection",
-    };
-    function handleSelect(ranges : any) {
-        setStartDate(ranges.selection.startDate);
-        setEndDate(ranges.selection.endDate);
-    }
+    // const [startDate, setStartDate] = useState(new Date());
+    // const [endDate, setEndDate] = useState(addDays(new Date(), 7));
+    // // const selectionRange : any = {
+    // //     startDate: startDate,
+    // //     endDate: endDate,
+    // //     key: "selection",
+    // // };
+    // function handleSelect(ranges : any) {
+    //     setStartDate(ranges.selection.startDate);
+    //     setEndDate(ranges.selection.endDate);
+    // }
     return (
         <Grid container justifyContent="center" style={{ width: "100%", margin: "auto" }}>
             <Stack sx={{maxWidth: "100%", '@media screen and (max-width: 600px)': {maxWidth: "300px"}}}>
 
             <Grid item xs={12} sm={6}>
-                <DateRangePicker
-                    ranges={[selectionRange]}
+                {/*<DateRangePicker*/}
+                {/*    ranges={[selectionRange]}*/}
 
-                    onChange={handleSelect}
-                    months={1}
-                    direction="horizontal"
-                    //showSelectionPreview={true}
-                    moveRangeOnFirstSelection={false}
-                    rangeColors={["#3d91ff"]}
-                    disabledDay={d => !isSameDay(d, new Date())}
-                    style={{ width: '100px' }}
-                />
+                {/*    onChange={handleSelect}*/}
+                {/*    months={1}*/}
+                {/*    direction="horizontal"*/}
+                {/*    //showSelectionPreview={true}*/}
+                {/*    moveRangeOnFirstSelection={false}*/}
+                {/*    rangeColors={["#3d91ff"]}*/}
+                {/*    disabledDay={d => !isSameDay(d, new Date())}*/}
+                {/*    style={{ width: '100px' }}*/}
+                {/*/>*/}
             </Grid>
             <Grid item xs={12} sm={6}>
                 <Button
